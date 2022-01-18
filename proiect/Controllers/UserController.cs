30016c1368f6a7f1011134342bf9962dbc645b7f
RaresCristescu <49509631+RaresCristescu@users.noreply.github.com>
2022-01-18 15:42:34 +0200

    Add project files.

---
+++ proiect/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using proiect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proiect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public static List<User> users = new List<User>
+        {
+            new User {Id=1,Name="Rares",UserName="admin",Email="admin@gmail.com",Telefon="0734951510"},
+            new User {Id=2,Name="Vasile",UserName="vsl",Email="vasile@gmail.com" ,Telefon="0734998510"},
+            new User {Id=3,Name="Luca",UserName="luk",Email="luk69@gmail.com",Telefon="0732251510" }
+        };
+        [HttpGet("getUsers")]
+        public List<User> Get()
+        {
+            return users;
+        }
+        [HttpGet("byId/{id}")]
+        public User GetUserByIp(int id)
+        {
+            return users.FirstOrDefault(s => s.Id.Equals(id));
+        }
+        [HttpGet("filter/{name}/{telefon}")]
+        public User GetWithFilter(string name, string telefon)
+        {
+            return users.FirstOrDefault(x => x.Name.Equals(name) && x.Telefon.Equals(telefon));
+        }
+
+    }
+}
---
+++ proiect/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proiect.Models
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        [StringLength(11)]
+        public string Telefon { get; set; }
+        /*public string Parola { get; set; }
+        
+        public string Rol { get; set; }
+        public string id_Adresa { get; set; }*/
+    }
+}

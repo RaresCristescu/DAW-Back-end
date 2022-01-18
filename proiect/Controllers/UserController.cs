bb2e83d5241b7c403ae1065948cce08d00419c65
RaresCristescu <49509631+RaresCristescu@users.noreply.github.com>
2022-01-18 16:52:24 +0200

    CRUD

--- proiect/Controllers/UserController.cs
+++ proiect/Controllers/UserController.cs
@@ -38,5 +38,52 @@ namespace proiect.Controllers
         {
             return users.FirstOrDefault(s => s.Id.Equals(id));
         }
+        [HttpGet("byIdFromQuery")]
+        public User GetUserByidFromQuery([FromQuery]int id)
+        {
+            return users.FirstOrDefault(s => s.Id.Equals(id));
+        }
+        [HttpGet("byIdFromQueryWithFiltre")]
+        public User GetUserByidFromQueryWithFiltre([FromQuery] int id,[FromQuery]string telefon)
+        {
+            return users.FirstOrDefault(s => s.Id.Equals(id) && s.Telefon.Equals(telefon));
+        }
+
+        [HttpPost("AddUser")]
+        public IActionResult Add(User user)
+        {
+            users.Add(user);
+
+            //return NotFound();
+            //return Forbid();
+
+            return Ok(users);
+        }
+        [HttpPost("addUserFromHeader")]
+        public IActionResult AddUserFromHeader([FromHeader]User user)//cica nu merge sa dia post din header
+        {
+            users.Add(user);
+            return Ok(users);
+        }
+
+        [HttpPost("addUserFromForm")]
+        public IActionResult AddUserFromForm([FromForm] User user)
+        {
+            users.Add(user);
+            return Ok(users);
+        }
+        [HttpPut("Update")]
+        public IActionResult Update(User user)
+        {
+            var userIndex = users.FindIndex((User _user) => _user.Id.Equals(user.Id));
+            users[userIndex] = user;
+            return Ok(users);
+        }
+        [HttpDelete]
+        public IActionResult Delete(User user)
+        {
+            users.Remove(user);
+            return Ok(users);
+        }
     }
 }
